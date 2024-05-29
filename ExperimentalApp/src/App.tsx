391aadfc1e7dcd0efcc34b8d6b9d290748dd10f7
JacobKosinski <jacob.kosinski@avncorp.com>
2024-05-29 08:03:37 -0400

    saving beggining changes

---
+++ ExperimentalApp/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from './components/NavBar';
+function App(){
+  return(
+    <>
+    <Nav />
+    <h1>Working Website trying to adjust</h1>
+  </>);
+}
+
+export default App;
